ad92d6b9200d7e2cbfbf4a2f6a3ad338d1a2c291
Igor Resman <igor.resman@gmail.com>
2018-12-30 16:29:27 +0100

    dodata pozadina rezultata i lopta nestaje kad je pogodi strelica

--- BubbleTrouble/pocetni.py
+++ BubbleTrouble/pocetni.py
@@ -7,6 +7,7 @@ from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QMainWindow
 
 from key_notifier import KeyNotifier
 from ballMovement import BallMovement
+from hitBall import HitBall
 
 
 class SimMoveDemo(QMainWindow):
@@ -19,13 +20,15 @@ class SimMoveDemo(QMainWindow):
         # pix33 = QPixmap('arrow.png')
         # pix44 = QPixmap('arrow.png')
         pix55 = QPixmap('ball.png')
-        self.labelispis = QLabel(self)
 
         self.label3 = QLabel(self)
         self.label4 = QLabel(self)
         self.label1 = QLabel(self)
         self.label2 = QLabel(self)
         self.label5 = QLabel(self)
+        self.label6 = QLabel(self)
+        self.labelispis = QLabel(self)
+        self.labelispis2 = QLabel(self)
 
         self.timerP1 = QTimer(self)
         self.timerP1.setInterval(2000)
@@ -39,16 +42,16 @@ class SimMoveDemo(QMainWindow):
 
         self.hitFloor = False
         self.hitSide = False
-        
+
         self.pix1 = pix11.scaledToHeight(84)
         self.pix1 = pix11.scaledToWidth(34)
 
         self.pix2 = pix22.scaledToHeight(204)
         self.pix2 = pix22.scaledToWidth(34)
 
-		self.pix5 = pix55.scaledToHeight(384)
+        self.pix5 = pix55.scaledToHeight(384)
         self.pix5 = pix55.scaledToWidth(54)
-		
+
         # self.pix3 = pix33.scaledToHeight(200)
         # self.pix3 = pix33.scaledToWidth(5)
         # self.pix4 = pix44.scaledToHeight(200)
@@ -66,11 +69,14 @@ class SimMoveDemo(QMainWindow):
         self.key_notifier.key_signal.connect(self.__update_position__)
         self.key_notifier.start()
 
-		
         self.ballMovement = BallMovement()
         self.ballMovement.ballMovementSignal.connect(self.moveBall)
         self.ballMovement.start()
-		
+
+        self.hitBall = HitBall()
+        self.hitBall.hitBallSignal.connect(self.checkHit)
+        self.hitBall.start()
+
     def __init_ui__(self):
         font = QtGui.QFont()
         font.setPointSize(40)
@@ -79,36 +85,45 @@ class SimMoveDemo(QMainWindow):
         self.labelispis.resize(100000, 100)
         self.labelispis.setGeometry(1500, 940, 400, 200)
 
+        self.labelispis2.setFont(font)
+        self.labelispis2.resize(100000, 100)
+        self.labelispis2.setGeometry(0, 940, 400, 200)
+
         self.label1.setPixmap(self.pix1)
         self.label1.setGeometry(1000, 900, 50, 50)
 
         self.label2.setPixmap(self.pix2)
         self.label2.setGeometry(900, 900, 50, 50)
 
-		
         self.label5.setPixmap(self.pix5)
         self.label5.setGeometry(50, 50, 50, 50)
-		
-        self.label3.resize(8, 900)
+
+        self.label3.resize(8, 950)
         pixmap1 = QtGui.QPixmap('arrow.png')
-        self.pixmap = pixmap1.scaled(8, 900)
+        self.pixmap = pixmap1.scaled(8, 950)
         self.label3.setPixmap(self.pixmap)
         self.label3.hide()
 
-        self.label4.resize(8, 900)
+        self.label4.resize(8, 950)
         pixmap1 = QtGui.QPixmap('arrow.png')
-        self.pixmap = pixmap1.scaled(8, 900)
+        self.pixmap = pixmap1.scaled(8, 950)
         self.label4.setPixmap(self.pixmap)
         self.label4.hide()
 
+        self.label6.resize(1920, 100)
+        pixmap1 = QtGui.QPixmap('ScoreBackground.jpg')
+        self.pixmapScore = pixmap1.scaled(1920, 150)
+        self.label6.setPixmap(self.pixmapScore)
+        self.label6.setGeometry(0, 950, 1920, 145)
+
         x = 'Player1:'
         y = 'Player2:'
-        score1 = 10
+        score1 = 10000
         score2 = 50000
 
         self.statusBar().setFont(font)
         self.labelispis.setText(str(x) + str(score1))
-        self.statusBar().showMessage(str(y) + str(score2))
+        self.labelispis2.setText(str(y) + str(score2))
         self.setWindowTitle('BubbleTrouble')
 
         self.show()
@@ -150,8 +165,8 @@ class SimMoveDemo(QMainWindow):
         elif key == Qt.Key_A:
             if rec2.x() > 15:
                 self.label2.setGeometry(rec2.x() - 15, rec2.y(), rec2.width(), rec2.height())
-				
-	def moveBall(self):
+
+    def moveBall(self):
         rec5 = self.label5.geometry()
 
         if (rec5.y() == 900):
@@ -173,8 +188,6 @@ class SimMoveDemo(QMainWindow):
         elif self.hitSide and self.hitFloor:
             self.label5.setGeometry(rec5.x() - 10, rec5.y() - 10, rec5.width(), rec5.height())
 
-
-
     def closeEvent(self, event):
         self.key_notifier.die()
 
@@ -183,6 +196,17 @@ class SimMoveDemo(QMainWindow):
 
     def hideArrow1(self):
         self.label3.hide()
+
+    def checkHit(self):
+        ballPosition = self.label5.geometry()
+        arrowPosition = self.label3.geometry()
+
+        if ballPosition.x() == arrowPosition.x():
+            self.label5.hide()
+            self.ballMovement.is_done = True
+        elif arrowPosition.x() == ballPosition.x():
+            self.label5.hide()
+            self.ballMovement.is_done = True
 
 
 if __name__ == '__main__':
